0e2278ba2fec93785cbed6a44055142ca7bec8f0
adamrd231 <adam.rdconcepts@gmail.com>
2020-12-02 20:50:11 -0800

    Begin react edible calculator, layout main screen with basic functions

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 export default function App() {
+
+  const [ thc, SetTHC ] = useState('');
+  const [ weight, Setweight ] = useState('');
+  const [ dosage, SetDosage ] = useState('');
+  const [ conversionFactor, setConversionFactor ] = useState('70');
+  const [ calculationForDosage, setcalculationForDosage ] = useState('');
+  const [ calculationForTotalTHC, setcalculationForTotalTHC ] = useState('');
+
+  const CalculateDosage = () => {
+    const numberOfSplits = (((thc * 0.01) * (weight * 1000)) * (0.01 * conversionFactor)) / dosage
+    const totalTHCinBatch = (thc * 0.01) * (weight * 1000) * (0.01 * conversionFactor)
+    setcalculationForTotalTHC(Number(totalTHCinBatch).toFixed())
+    setcalculationForDosage(Number(numberOfSplits).toFixed())
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="?"
+      />
+
+      <Text>THC</Text>
+      <TextInput 
+        value={thc} 
+        onChangeText={thc => SetTHC(thc)}
+      />
+
+      <Text>weight</Text>
+      <TextInput 
+        value={weight} 
+        onChangeText={weight => Setweight(weight)}
+      />
+
+      <Text>Number of Units in Batch</Text>
+      <TextInput 
+        value={dosage} 
+        onChangeText={dosage => SetDosage(dosage)}
+      />
+
+      <Text>Conversion Factor: {Number(conversionFactor).toFixed()}</Text>
+      <Slider 
+        value={conversionFactor}
+        onSlidingComplete={(conversionFactor) => setConversionFactor(Number(conversionFactor).toFixed())}
+        style={{width: 200, height: 30}}
+        minimumValue={0}
+        maximumValue={100}
+        minimumTrackTintColor="#efefef"
+        maximumTrackTintColor="#000000"
+      />
+
+      <Button
+        title="Calculate"
+        onPress={CalculateDosage}
+      />
+      <Text>This batch has {calculationForTotalTHC}mg of THC total.</Text>
+      <Text>{dosage} units with about {calculationForDosage}mg of THC each.</Text>
       <StatusBar style="auto" />
     </View>
   );
